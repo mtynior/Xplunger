276f6d4460e54ba31ace3af4f9f73a04a8edc511
Michal Tynior <michal.git@tynior.com>
2023-09-04 17:41:31 +0200

    Initial Commit

---
+++ Xplunger/XplungerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XplungerApp.swift
+//  Xplunger
+//
+//  Created by Michal on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct XplungerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
