054cb1489a6a1d956e8eec948cdf7e9a287b79f2
Michal Tynior <michal.git@tynior.com>
2023-09-07 19:38:28 +0200

    Add menu icon

--- Xplunger/Sources/App/XplungerApp.swift
+++ Xplunger/Sources/App/XplungerApp.swift
@@ -10,10 +10,14 @@ import SwiftUI
 @main
 struct XplungerApp: App {
     var body: some Scene {
-        MenuBarExtra("Xplunger", systemImage: "shower.fill") {
+        MenuBarExtra {
             AppMenu()
+        } label: {
+            let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .black)
+            let image = NSImage(named: "plunger")
+            let updateImage = image?.withSymbolConfiguration(configuration)
+            Image(nsImage: updateImage!)
         }
         .menuBarExtraStyle(.window)
-        
     }
 }
